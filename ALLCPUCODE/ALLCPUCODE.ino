c34fd88b7bc42afde2c1f32a7e45567394fcb323
Morrissey, Julia Ann <jmorrissey6@gatech.edu>
2021-02-25 17:44:58 -0500

    New Wifi Server Code
    Updates wifi code

--- ALLCPUCODE/ALLCPUCODE.ino
+++ ALLCPUCODE/ALLCPUCODE.ino
@@ -67,36 +67,46 @@ boolean GPSstatus = true;
 const char* ssid = "ESP32";  // Enter SSID here
 const char* password = "12345678";  //Enter Password here
 
-/* Put IP Address details */
-IPAddress local_ip(192,168,1,1);
-IPAddress gateway(192,168,1,1);
-IPAddress subnet(255,255,255,0);
 
 WebServer server(80);
+String header;
+String output26State = "off";
+String output27State = "off";
+ 
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
+
 
 void setup() {
   //Communication stuff
   Serial.begin(115200);
   Wire.begin(I2C_SDA, I2C_SCL); //Can choose pretty much any pins on the esp32 for I2C
   Wire.setClock(400000);
-
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);
+  
   //Set pin modes
   pinMode(HEATER_OUTPUT, OUTPUT);
   pinMode(BATT_1, INPUT);
   pinMode(BATT_2, INPUT);
   pinMode(BATT_3, INPUT);
 
-  //wifi setup
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
   WiFi.softAP(ssid, password);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  delay(100);
-
-  server.on("/", handle_OnConnect);
-  server.on("/launched", handle_launched);
-  server.onNotFound(handle_NotFound);
-  
+ 
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+   
   server.begin();
-  Serial.println("HTTP server started");
 
   //Initialize variables if necessary
   heater_state = 0;
@@ -201,61 +211,92 @@ void loop() {
   heater();
   GPS();    
   readBatteryVoltage();
-  server.handleClient();
-  
+  wifi();
 }
 
-void handle_OnConnect() {
+void wifi() {
   server.send(200, "text/html", SendHTML(launched, SDstatus, GPSstatus, batt_1_voltage, batt_2_voltage, batt_3_voltage, myGPS.getSIV())); 
-}
+  WiFiClient client = server.available();   // Listen for incoming clients
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+             
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            }
+             
+            // Display the HTML web page  
+            client.println("<!DOCTYPE html> <html>\n");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n");
+            client.println("<title>Dashboard</title>\n");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n");
+            client.println("body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n");
+            client.println("p {font-size: 14px;color: #888;margin-bottom: 10px;}\n");
+            client.println("</style>\n");
+            client.println("</head>\n");
+            client.println("<body>\n");
+            client.println("<h1>ESP32 Web Server</h1>\n");
+            client.println("<h3>Lightning at the Edge of Space VIP</h3>\n");
+              if(launched)
+                {client.println("<p>Launched</p>\n");}
+              else
+                {client.println("<p>Not Launched</p>\n");}
 
-void handle_launched() {
-  launched = true;
-  Serial.println("launched : TRUE");
-  server.send(200, "text/html", SendHTML(launched, SDstatus, GPSstatus, batt_1_voltage, batt_2_voltage, batt_3_voltage, myGPS.getSIV()));
-
-}
-
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(boolean launched, boolean SDstatus, boolean GPSstatus, float batt_1_voltage, float batt_2_voltage, float batt_3_voltage, byte sat){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>Dashboard</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<h1>ESP32 Web Server</h1>\n";
-  ptr +="<h3>Lightning at the Edge of Space VIP</h3>\n";
-  
-  if(launched)
-    {ptr +="<p>Launched</p>\n";}
-  else
-    {ptr +="<p>Not Launched</p>\n";}
-
-  if(SDstatus)
-    {ptr +="<p>SD is connected and writing to files successfully</p>\n";}
-  else
-    {ptr +="<p>SD writing is not properly working</p>\n";}
-  if(GPSstatus)
-    {ptr +="<p>GPS is connected and working properly</p>\n";}
-  else
-    {ptr +="<p>GPS is not properly working</p>\n";}
+              if(SDstatus)
+                {client.println("<p>SD is connected and writing to files successfully</p>\n");}
+              else
+                {client.println("<p>SD writing is not properly working</p>\n");}
+              if(GPSstatus)
+                {client.println("<p>GPS is connected and working properly</p>\n");}
+              else
+                {client.println("<p>GPS is not properly working</p>\n");}
     
-  ptr +=  "<p>Battery 1 voltage: " + String(batt_1_voltage) + "</p>\n";
-  ptr +=  "<p>Battery 2 voltage: " + String(batt_2_voltage) + "</p>\n";
-  ptr +=  "<p>Battery 3 voltage: " + String(batt_3_voltage) + "</p>\n";
-  ptr +=  "<p>GPS satellite: " + String(sat) + "</p>\n";
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
-  
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
+
+
+
+
 
 void heater() { //Contains the loop code for the heater system
   if(millis() >= next_heater_check) {
