1afdf12e16f220efd901504ec074ecc0d8a61e64
tomh671 <57495484+tomh671@users.noreply.github.com>
2021-03-18 19:01:07 -0400

    commentador

--- ALLCPUCODE/ALLCPUCODE.ino
+++ ALLCPUCODE/ALLCPUCODE.ino
@@ -1,4 +1,4 @@
-/* Authors: 
+g/* Authors: 
  * Fall 2020
  */
 //#include stuff
@@ -430,40 +430,40 @@ void geofenceCheck() {    //Run every time there's new GPS data available
     triggerCutdown();
   }
  
-  pastLatitudeDisplacement[avgFramePos] = latitude - lastLatitude;
-  pastLongitudeDisplacement[avgFramePos] = longitude - lastLongitude;
-  avgFramePos++;
-  avgFramePos = avgFramePos % DISPLACEMENT_AVG_FRAME;
-
-  //Determine average latitude and longitude drift rate
-  float latSum, longSum;
-  for(int i = 0; i < DISPLACEMENT_AVG_FRAME; i++){
-    latSum = latSum + pastLatitudeDisplacement[i];
-    longSum = longSum + pastLongitudeDisplacement[i];
-  }
-  float latDriftRate = latSum / (DISPLACEMENT_AVG_FRAME * GPS_CHECK_TIME);
-  float longDriftRate = longSum / (DISPLACEMENT_AVG_FRAME * GPS_CHECK_TIME);
-
-  //Find predicted latitude and longitude of landing position
-  float latPredicted = latDriftRate * (altitude / 1000 / DESCENT_RATE); //Altitude is given by the gps in mm
-  float longPredicted = longDriftRate * (altitude / 1000 / DESCENT_RATE);
-
-  //Find if cutdown is required
-  if( (latPredicted / 10000000) < 30.736 ) { //latitude and longitude are given by the GPS in degrees * 10^7
-    triggerCutdown();
-  } else if( (latPredicted / 10000000) < 32.851 ) {
-    if( (longPredicted / 10000000) > ((latPredicted / 10000000 / 2.2175) - 96.017) ) {
-      triggerCutdown();
-    }
-  } else if( (latPredicted / 10000000) < 35.031 ) {
-    if( (longPredicted / 10000000) > ((latPredicted / 10000000 / .63537) - 132.61) ) {
-      triggerCutdown();
-    }
-  } else {
-    if( (longPredicted / 10000000) > -77.468 ) {
-      triggerCutdown();
-    }
-  }
+//  pastLatitudeDisplacement[avgFramePos] = latitude - lastLatitude;
+//  pastLongitudeDisplacement[avgFramePos] = longitude - lastLongitude;
+//  avgFramePos++;
+//  avgFramePos = avgFramePos % DISPLACEMENT_AVG_FRAME;
+//
+//  //Determine average latitude and longitude drift rate
+//  float latSum, longSum;
+//  for(int i = 0; i < DISPLACEMENT_AVG_FRAME; i++){
+//    latSum = latSum + pastLatitudeDisplacement[i];
+//    longSum = longSum + pastLongitudeDisplacement[i];
+//  }
+//  float latDriftRate = latSum / (DISPLACEMENT_AVG_FRAME * GPS_CHECK_TIME);
+//  float longDriftRate = longSum / (DISPLACEMENT_AVG_FRAME * GPS_CHECK_TIME);
+//
+//  //Find predicted latitude and longitude of landing position
+//  float latPredicted = latDriftRate * (altitude / 1000 / DESCENT_RATE); //Altitude is given by the gps in mm
+//  float longPredicted = longDriftRate * (altitude / 1000 / DESCENT_RATE);
+//
+//  //Find if cutdown is required
+//  if( (latPredicted / 10000000) < 30.736 ) { //latitude and longitude are given by the GPS in degrees * 10^7
+//    triggerCutdown();
+//  } else if( (latPredicted / 10000000) < 32.851 ) {
+//    if( (longPredicted / 10000000) > ((latPredicted / 10000000 / 2.2175) - 96.017) ) {
+//      triggerCutdown();
+//    }
+//  } else if( (latPredicted / 10000000) < 35.031 ) {
+//    if( (longPredicted / 10000000) > ((latPredicted / 10000000 / .63537) - 132.61) ) {
+//      triggerCutdown();
+//    }
+//  } else {
+//    if( (longPredicted / 10000000) > -77.468 ) {
+//      triggerCutdown();
+//    }
+//  }
 }
 
 void checkTimeCutdown() {
